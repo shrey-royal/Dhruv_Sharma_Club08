beb1f360e8736f65dc91515e23a1c239c8614bd0
Shrey K <srkkadia@gmail.com>
2024-04-24 14:13:04 +0530

    Abstract Class

--- OOPs/Abstraction/Abstract_Class.cpp
+++ OOPs/Abstraction/Abstract_Class.cpp
@@ -14,3 +14,82 @@ In this scenario, you can use an abstract class called "Vehicle" as a blueprint 
 Whichever class will have the pure virtual function, that class will become the abstract class. and whichever class will inherit the abstract class, that class will become the concrete class. and the concrete class must have to override the pure virtual function of the abstract class.
 
 */
+
+#include<iostream>
+using namespace std;
+
+class Vehicle {
+private:
+    string brand;
+    string model;
+    int maxSpeed;
+public:
+    Vehicle(string b, string m, int speed) : brand(b), model(m), maxSpeed(speed) {}
+
+    virtual void startEngine() = 0;
+    virtual void stopEngine() = 0;
+
+    void display() {
+        cout << "Brand: " << brand << ", Model: " << model << ", MaxSpeed: " << maxSpeed << endl;
+    }
+};
+
+class Car : public Vehicle {
+public:
+    Car(string brand, string model, int maxSpeed) : Vehicle(brand, model, maxSpeed) {}
+
+    void startEngine() override {
+        cout << "Car's engine started!" << endl;
+    }
+
+    void stopEngine() override {
+        cout << "Car's engine stopped!" << endl;
+    }
+};
+
+class MotorCycle : public Vehicle {
+public:
+    MotorCycle(string brand, string model, int maxSpeed) : Vehicle(brand, model, maxSpeed) {}
+
+    void startEngine() override {
+        cout << "MotorCycle's engine started!" << endl;
+    }
+
+    void stopEngine() override {
+        cout << "MotorCycle's engine stopped!" << endl;
+    }
+};
+
+class Bicycle : public Vehicle {
+public:
+    Bicycle(string brand, string model, int maxSpeed) : Vehicle(brand, model, maxSpeed) {}
+
+    void startEngine() override {
+        cout << "Bicycles does not have any engines!" << endl;
+    }
+
+    void stopEngine() override {
+        cout << "Bicycles does not have any engines!" << endl;
+    }
+};
+
+int main() {
+    Car c("Tata", "Harrier", 240);
+    MotorCycle bike("Royal Enfield", "Himalayan", 180);
+    Bicycle cycle("Herculus", "Captain Shakti", 20);
+
+    c.startEngine();
+    c.stopEngine();
+    c.display();
+    
+    bike.startEngine();
+    bike.stopEngine();
+    bike.display();
+
+    cycle.startEngine();
+    cycle.stopEngine();
+    cycle.display();
+
+
+    return 0;
+}
